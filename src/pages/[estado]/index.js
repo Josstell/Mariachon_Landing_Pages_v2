5bf97b70e030a1ae23131ef4adbcf528dbe4a579
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-04-30 17:48:27 -0500

    lint- prettier lintstage

--- src/pages/[estado]/index.js
+++ src/pages/[estado]/index.js
@@ -1,56 +1,56 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useRouter } from "next/router"
-import Head from "next/head"
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 
-import Dispo from "../../components/LandingPages/Dispo"
-import FooterLanding from "../../components/LandingPages/Footer"
-import Header from "../../components/LandingPages/Header"
-import Section01 from "../../components/LandingPages/Section01"
-import Section02 from "../../components/LandingPages/Section02"
+import Dispo from '../../components/LandingPages/Dispo'
+import FooterLanding from '../../components/LandingPages/Footer'
+import Header from '../../components/LandingPages/Header'
+import Section01 from '../../components/LandingPages/Section01'
+import Section02 from '../../components/LandingPages/Section02'
 
-import { database } from "../../helpers/database"
+import { database } from '../../helpers/database'
 
 const region = ({ data }) => {
-	const router = useRouter()
-	//const recaptchaRef = useRef(null)
+  const router = useRouter()
+  //const recaptchaRef = useRef(null)
 
-	if (router.isFallback) {
-		return <div>CARGANDO... </div>
-	}
+  if (router.isFallback) {
+    return <div>CARGANDO... </div>
+  }
 
-	return (
-		<>
-			<Head>
-				<title>Mariachis en {data.region}</title>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<link rel="icon" href="/icons/logomariachon.png" />
-			</Head>
-			<Header data={data.header} icons={data.icons} region={data.region} />
+  return (
+    <>
+      <Head>
+        <title>Mariachis en {data.region}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/icons/logomariachon.png" />
+      </Head>
+      <Header data={data.header} icons={data.icons} region={data.region} />
 
-			<Section01 data={data.section_01} />
-			<Section02 data={data.section_02} />
+      <Section01 data={data.section_01} />
+      <Section02 data={data.section_02} />
 
-			<Dispo data={data.pre_footer} />
-			<FooterLanding data={data.footer} icons={data.icons} />
-		</>
-	)
+      <Dispo data={data.pre_footer} />
+      <FooterLanding data={data.footer} icons={data.icons} />
+    </>
+  )
 }
 
 export async function getStaticPaths() {
-	const paths = await database.map((path) => ({
-		params: { estado: path.region.toString() },
-	}))
+  const paths = await database.map((path) => ({
+    params: { estado: path.region.toString() },
+  }))
 
-	return { paths, fallback: false }
+  return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
-	const data = await database.filter(
-		(est) => est.region.toString() === params.estado
-	)
-	return {
-		props: { data: data[0] }, // will be passed to the page component as props
-	}
+  const data = await database.filter(
+    (est) => est.region.toString() === params.estado
+  )
+  return {
+    props: { data: data[0] }, // will be passed to the page component as props
+  }
 }
 
 export default region
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,29 @@
-import Link from "next/link"
-import Head from "next/head"
+import Link from 'next/link'
+import Head from 'next/head'
 
-import { database } from "../helpers/database"
+import { database } from '../helpers/database'
 
 export async function getStaticProps() {
-	return {
-		props: {
-			data: database,
-		},
-	}
+  return {
+    props: {
+      data: database,
+    },
+  }
 }
 
 export default function Home({ data }) {
-	return (
-		<>
-			<Head>
-				<title>Mariachon</title>
-				<link rel="icon" href="/icons/logomariachon.png" />
-			</Head>
-			<Link
-				className="text-3xl font-bold underline"
-				href={`/${data[0].region}`}
-			>
-				queretaro
-			</Link>
-		</>
-	)
+  return (
+    <>
+      <Head>
+        <title>Mariachon</title>
+        <link rel="icon" href="/icons/logomariachon.png" />
+      </Head>
+      <Link
+        className="text-3xl font-bold underline"
+        href={`/${data[0].region}`}
+      >
+        queretaro
+      </Link>
+    </>
+  )
 }
