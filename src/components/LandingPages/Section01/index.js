3bca8d72ab560e50c6b016d8067fe598898563d0
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-05-16 12:21:28 -0500

    Correccion h1 seccion01

--- src/components/LandingPages/Section01/index.js
+++ src/components/LandingPages/Section01/index.js
@@ -1,22 +1,22 @@
-import React from "react"
+import React from 'react'
 
 const Section01 = ({ data }) => {
-	return (
-		<div className="w-full h-[100vh] flex flex-col justify-center items-center bg-[#05117F]">
-			<div className="flex flex-col lg:flex-row">
-				<h1 className=" font-sans text-white font-black text-center text-[50px] 2xl:text-9xl ">
-					{data.title.textT.a}&nbsp;
-				</h1>
-
-				<h1 className=" font-sans text-white font-black text-center text-[50px] 2xl:text-9xl">
-					{data.title.textT.b}
-				</h1>
-			</div>
-			<p className="font-sans  w-11/12 px-5  text-white text-center text-[30px] mt-10 2xl:text-5xl">
-				{data.paragraph.textP}
-			</p>
-		</div>
-	)
+  return (
+    <div className="w-full h-[100vh] flex flex-col justify-center items-center bg-[#05117F]">
+      <div className="flex flex-col lg:flex-row">
+        <h1 className=" font-sans text-white font-black text-center text-[50px] 2xl:text-9xl ">
+          {data.title.textT.a}
+          <br className="md:hidden" />
+          {data.title.textT.b}
+          <br className="md:hidden" />
+          {data.title.textT.c}
+        </h1>
+      </div>
+      <p className="font-sans  w-11/12 px-5  text-white text-center text-[30px] mt-10 2xl:text-5xl">
+        {data.paragraph.textP}
+      </p>
+    </div>
+  )
 }
 
 export default Section01
--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,107 +1,107 @@
 export const database = [
-	{
-		id: 0,
-		region: "Queretaro",
-		header: {
-			img: {
-				url: "/images/queretaro/mariachisenqueretaro.jpg",
-				metadata: {
-					description: "Mariachis en Queretaro",
-				},
-			},
-			form: {
-				bgColor: "#EDEDEDDF",
-				//bgColor: "#000000FF",
-				title: {
-					textT: "Contrata Fácil y rápido",
-					colorT: "#96090C",
-					fontFamily: "font-sans",
-				},
-				button: {
-					textB: "Disponibilidad",
-					colorB: "#E66400",
-					fontFamily: "font-sans",
-				},
-				cover: "Cobertura ciudad de Santiago de Queretaro",
-			},
-		},
-		section_01: {
-			bgColor: "#05117F",
-			title: {
-				textT: { a: "Mariachis", b: "en Queretaro" },
-				colorT: "#FFF",
-			},
-			paragraph: {
-				textP:
-					"Mariachon plataforma líder para la contratación de mariachis en tu ciudad.",
-				colorP: "#FFF",
-			},
-		},
-		section_02: {
-			colorB: "#FFF",
-			title: {
-				a: {
-					textT: "Mariachis",
-					colorT: "#960909",
-				},
-				b: {
-					textT: "responsables y de confianza",
-					colorT: "#000",
-				},
-			},
-			paragraph: {
-				textP:
-					"Encuentra precios y variedad de grupos según la calidad, performance e imagen.",
-				colorP: "#000",
-			},
-		},
-		pre_footer: {
-			image: {
-				url: "/images/queretaro/preciosmariachisenqueretaro.jpg",
-				metadata: {
-					description: "Mariachis en Queretaro disponibilidad",
-				},
-			},
-			button: {
-				textB: "Disponibilidad",
-				color_border: "#FFF",
-			},
-		},
-		footer: {
-			bgColor: "#FFF",
-			copyright: {
-				textC:
-					"Todos los derechos reservados © 2022 Mariachon. Política de Privacidad",
-				colorC: "#000",
-			},
-		},
-		icons: {
-			logo: {
-				url: "/icons/logomariachon.png",
-				metadata: { description: "Logo Mariachon" },
-			},
-			facebook: {
-				url: "/icons/facebook.svg",
-				metadata: {
-					description: "Facebook Logo",
-					page: "https://www.facebook.com/",
-				},
-			},
-			instagram: {
-				url: "/icons/instagram.svg",
-				metadata: {
-					description: "Instagram Logo",
-					page: "https://www.instagram.com/",
-				},
-			},
-			whatsapp: {
-				url: "/icons/whatsapp.svg",
-				metadata: {
-					description: "Whatsapp Logo",
-					page: "https://www.whatsapp.com/?lang=es",
-				},
-			},
-			youtube: { url: "", metadata: { description: "", page: "" } },
-		},
-	},
+  {
+    id: 0,
+    region: 'Queretaro',
+    header: {
+      img: {
+        url: '/images/queretaro/mariachisenqueretaro.jpg',
+        metadata: {
+          description: 'Mariachis en Queretaro',
+        },
+      },
+      form: {
+        bgColor: '#EDEDEDDF',
+        //bgColor: "#000000FF",
+        title: {
+          textT: 'Contrata Fácil y rápido',
+          colorT: '#96090C',
+          fontFamily: 'font-sans',
+        },
+        button: {
+          textB: 'Disponibilidad',
+          colorB: '#E66400',
+          fontFamily: 'font-sans',
+        },
+        cover: 'Cobertura ciudad de Santiago de Queretaro',
+      },
+    },
+    section_01: {
+      bgColor: '#05117F',
+      title: {
+        textT: { a: 'Mariachis ', b: 'en ', c: 'Queretaro' },
+        colorT: '#FFF',
+      },
+      paragraph: {
+        textP:
+          'Mariachon plataforma líder para la contratación de mariachis en tu ciudad.',
+        colorP: '#FFF',
+      },
+    },
+    section_02: {
+      colorB: '#FFF',
+      title: {
+        a: {
+          textT: 'Mariachis',
+          colorT: '#960909',
+        },
+        b: {
+          textT: 'responsables y de confianza',
+          colorT: '#000',
+        },
+      },
+      paragraph: {
+        textP:
+          'Encuentra precios y variedad de grupos según la calidad, performance e imagen.',
+        colorP: '#000',
+      },
+    },
+    pre_footer: {
+      image: {
+        url: '/images/queretaro/preciosmariachisenqueretaro.jpg',
+        metadata: {
+          description: 'Mariachis en Queretaro disponibilidad',
+        },
+      },
+      button: {
+        textB: 'Disponibilidad',
+        color_border: '#FFF',
+      },
+    },
+    footer: {
+      bgColor: '#FFF',
+      copyright: {
+        textC:
+          'Todos los derechos reservados © 2022 Mariachon. Política de Privacidad',
+        colorC: '#000',
+      },
+    },
+    icons: {
+      logo: {
+        url: '/icons/logomariachon.png',
+        metadata: { description: 'Logo Mariachon' },
+      },
+      facebook: {
+        url: '/icons/facebook.svg',
+        metadata: {
+          description: 'Facebook Logo',
+          page: 'https://www.facebook.com/',
+        },
+      },
+      instagram: {
+        url: '/icons/instagram.svg',
+        metadata: {
+          description: 'Instagram Logo',
+          page: 'https://www.instagram.com/',
+        },
+      },
+      whatsapp: {
+        url: '/icons/whatsapp.svg',
+        metadata: {
+          description: 'Whatsapp Logo',
+          page: 'https://www.whatsapp.com/?lang=es',
+        },
+      },
+      youtube: { url: '', metadata: { description: '', page: '' } },
+    },
+  },
 ]
