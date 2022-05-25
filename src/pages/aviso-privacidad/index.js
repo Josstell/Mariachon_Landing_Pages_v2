7e59937b8595f09a67a808fd38f601f4c0c40639
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-05-25 12:28:43 -0500

    aviso-privacidad 4

--- src/pages/aviso-privacidad/index.js
+++ src/pages/aviso-privacidad/index.js
@@ -26,14 +26,14 @@ const aviso_privacidad = ({ data }) => {
           “Empresa como tal” el Mariachon con domicilio calle 7 sur no. 309 San
           Juan Tianguismanalco, Puebla.
         </h4>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Aviso dirigido a los titulares de datos personales que obran en el
           sitio web de http://mariachon.com/ y él no tratamiento de datos
           personales sensibles. http://mariachon.com/ es responsable de recabar
           sus datos personales, del uso que se le dé a los mismos y de la
           protección de dichos datos.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Así mismo hace de su conocimiento que los datos personales
           proporcionados por usted de manera directa o por cualquier medio de
           contacto incluyendo los sensibles, que actualmente o en el futuro
@@ -42,7 +42,7 @@ const aviso_privacidad = ({ data }) => {
           relacionados con los que han sido requerido por usted como cliente.
         </p>
         <h3 className="text-2xl text-center py-5 font-bold">Finalidades</h3>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           <span>Mariachon</span> dará cumplimiento a obligaciones contraídas;
           evaluar la calidad del servicio prestado, realizar estudios internos
           sobre hábitos de servicio, contactarlo en caso de requerirse
@@ -52,7 +52,7 @@ const aviso_privacidad = ({ data }) => {
           finalidad principal la de ofrecer nuestra variedad de servicios, así
           como publicidad e información relevante que puedan ser de su interés.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Para las finalidades antes mencionadas, requerimos obtener los
           siguientes datos personales:
         </p>
@@ -63,7 +63,7 @@ const aviso_privacidad = ({ data }) => {
           <li>Dirección</li>
           <li>Código Postal.</li>
         </ul>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Usted tiene derecho de acceder, rectificar y cancelar sus datos
           personales, así como de oponerse al tratamiento de estos o revocar el
           consentimiento. Los titulares podrán ejercer los derechos que les
@@ -73,21 +73,21 @@ const aviso_privacidad = ({ data }) => {
           “Mariachon, independiente del medio utilizado por los titulares,
           deberán realizar lo siguiente:
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Incluir el nombre y firma autógrafa del titular, así como un domicilio
           u otro medio para comunicarle la respuesta a su solicitud.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Acompañar los documentos oficiales que acrediten la identidad del
           titular. Incluir una descripción clara y precisa de los datos
           personales respecto de los cuales ejercitará los derechos que les
           confiere la Ley.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Incluir cualquier elemento o documento que facilite la localización de
           los datos personales de que se traten.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           “La empresa Mariachon” le recomienda a usted como cliente que
           actualice sus datos cada vez que éstos sufran alguna modificación,
           esto permitirá brindarle un servicio más personalizado;
@@ -97,14 +97,14 @@ const aviso_privacidad = ({ data }) => {
           Protección de Datos Personales en Posposición de los Particulares y su
           Reglamento.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           La confidencialidad de los datos está garantizada y los mismos están
           protegidos por medidas de seguridad administrativas, técnicas y
           físicas, para evitar su daño, pérdida, alteración, destrucción, uso,
           acceso o divulgación indebida. Únicamente las personas autorizadas
           tendrán acceso a sus datos.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Así mismo, le informamos que sus datos personales pueden ser
           transferidos y tratados dentro y fuera del país, por personas
           distintas a esta “empresa”. En ese sentido, su información puede ser
@@ -112,20 +112,20 @@ const aviso_privacidad = ({ data }) => {
           servicios que estén relacionados con los que han sido contratados por
           usted como cliente.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Si usted no manifiesta su oposición para que sus datos personales sean
           transferidos o utilizados a los servicios del sitio
           https://mariachon.com/ se entenderá que ha otorgado su consentimiento
           para ello.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Finalmente nos reservamos el derecho bajo su exclusiva discreción de
           cambiar, modificar, agregar, actualizar o eliminar al presente aviso
           de privacidad, para la atención de novedades legislativas o
           jurisprudenciales, políticas internas, nuevos requerimientos para la
           prestación u ofrecimiento de nuestros servicios.
         </p>
-        <p className="text-justify py-2">
+        <p className="text-justify text-lg py-2">
           Cualquier modificación a este aviso de privacidad podrá consultarlo en
           el sitio web: http://mariachon.com/ [sección aviso de privacidad].
           Para cualquier duda en materia de privacidad de datos personales, no
