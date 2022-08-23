f33e6a185617f9fc618acf9b2f149997fdcc2d40
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2022-08-23 15:36:23 -0500

    api add mariachi added 4

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+import handlerCors from 'src/helpers/api/allowCors'
+
+export default handlerCors().post(async (req, res) => {
+  res.status(200).json({ name: req.body.name })
+})
